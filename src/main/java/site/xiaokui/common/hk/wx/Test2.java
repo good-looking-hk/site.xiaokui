94e77fbc795c2493a46c18337c7fcd359857b0d8
good-looking-hk <good-looking-hk>
2020-04-30 09:11:19 +0800

    添加微信公众号机器人

---
+++ src/main/java/site/xiaokui/common/hk/wx/Test2.java
@@ -0,0 +1,25 @@
+package site.xiaokui.common.hk.wx;
+
+/**
+ * @author HK
+ * @date 2020-04-29 16:24
+ */
+public class Test2 {
+
+    public static void main(String[] args) {
+        method(null);
+    }
+
+    public static void method(String param) {
+        switch (param) { // 肯定不是进入这里
+            case "sth":
+                System.out.println("it's sth");
+                break;
+            // 也不是进入这里
+            case "null":
+                System.out.println("it's null");
+                break;
+            // 也不是进入这里 default: System.out.println("default");
+        }
+    }
+}
